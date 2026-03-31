987dba4f15297ce89f2fc3ae287040869cf35df1
MarcoZ23 <zollomarco23@gmail.com>
2026-03-31 21:42:40 +0200

    add api for random email

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hello");
+/* 
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/ 
+
+
+const email = "https://flynn.boolean.careers/exercises/api/random/mail" 
+
+
+fetch(email)
+.then (response => response.json())
+.then (data => {
+    console.log(data);
+    
+})
