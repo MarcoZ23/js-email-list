576e69cbc1f035d4b5b535c639c38c44b58885d4
MarcoZ23 <zollomarco23@gmail.com>
2026-03-31 22:57:07 +0200

    print email list on page

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 const email = "https://flynn.boolean.careers/exercises/api/random/mail" 
 const emailList = []
 
-
+const mailList = document.getElementById("mail-list")
     
 
 for (let i = 0; i < 10; i++) {
@@ -17,9 +17,14 @@ for (let i = 0; i < 10; i++) {
     .then (data => {
         console.log(data.response);
     emailList.push(data.response)
-    
+const li = document.createElement("li")
+    li.innerHTML = data.response
+    mailList.appendChild(li)
 })
 }
 
 console.log(emailList);
 
+
+    
+    
